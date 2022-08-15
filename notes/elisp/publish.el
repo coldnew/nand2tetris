2444dd577b71f411bf37e17c2a6e1c9731d8ce5e
Yen-Chin, Lee <coldnew.tw@gmail.com>
2022-08-15 15:50:17 +0800

    elisp: fix install pacakges
    Signed-off-by: Yen-Chin, Lee <coldnew.tw@gmail.com>

--- notes/elisp/publish.el
+++ notes/elisp/publish.el
@@ -42,12 +42,13 @@
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
 			 ("elpa" . "https://elpa.gnu.org/packages/")))
 
-(setq package-load-list '((htmlize t)))
-
 (package-initialize)
 
-;; (unless package-archive-contents
-;;   (package-refresh-contents))
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;;; Install packages
+(package-install 'htmlize)
 
 ;;; Initialize org-mode
 
